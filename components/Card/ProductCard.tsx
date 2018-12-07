37e55e3203ce31716ec78f96b6d2927496d411cf
SamuelMuloki <samuellebron0@gmail.com>
2018-12-07 17:03:25 +0300

    Update product card

--- components/Card/ProductCard.tsx
+++ components/Card/ProductCard.tsx
@@ -16,7 +16,7 @@ interface ProductCardProps {
 const ProductCard = ({ images }: ProductCardProps) => {
   return (
     <Grid container spacing={8}>
-      <Grid item lg={3} sm={4} xs={12}>
+      <Grid item lg={3} md={3} sm={4} xs={12}>
         <List>
           <ListItem>
             <ListItemText primary="Filter" />
@@ -24,13 +24,15 @@ const ProductCard = ({ images }: ProductCardProps) => {
           <Divider />
         </List>
       </Grid>
-      {images.products.map(imgs => {
-        return (
-          <Grid key={imgs._id} item xs={6} sm={4} md={3} lg={2} zeroMinWidth>
-            <SingleCard images={imgs} />
-          </Grid>
-        )
-      })}
+      <Grid container item lg={9} md={9}>
+        {images.products.map(imgs => {
+          return (
+            <Grid key={imgs._id} item xs={6} sm={4} md={4} lg={3} zeroMinWidth>
+              <SingleCard images={imgs} />
+            </Grid>
+          )
+        })}
+      </Grid>
     </Grid>
   )
 }
